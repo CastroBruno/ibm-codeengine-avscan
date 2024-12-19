d2411ad89418446c5ee39f4c96e081b8a7fae968
Bruno Castro <castro.bruno@outlook.com.br>
2024-12-19 00:42:00 -0300

    fix

--- docker/av-job/index.js
+++ docker/av-job/index.js
@@ -7,8 +7,9 @@ require("dotenv").config({
 const fs = require("fs");
 var config = {
     endpoint: process.env.COS_ENDPOINT,
-    apiKeyId: "ApiKey-8a911bf0-af9a-4543-997c-fbfba2b7da76",
-    serviceInstanceID: "ServiceId-33e48a1d-276b-4255-98d2-5258478ca56e",
+    apiKeyId: process.env.COS_SECRET_APIKEY,
+    serviceInstanceId: process.env.COS_SECRET_RESOURCE_INSTANCE_ID,
+    ibmAuthEndpoint: "https://iam.cloud.ibm.com/identity/token",
   };
 var cosClient = new myCOS.S3(config);
 const NodeClam = require('clamscan');
